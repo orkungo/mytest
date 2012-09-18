d7b62f4d61cc63f8442c0b5ef67c6953eff2e336
orkun <orkun@ubuntu.(none)>
2012-09-18 04:02:27 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytest::Application.config.secret_token = 'ab78636ee455183ea64e09c4e70b41da5f67e51ca76c904343d1f3ebd99dbcdeca39f2b66947ce45554b5c77f322af49ea2b2f7756c3357ff54fe928f865f024'
